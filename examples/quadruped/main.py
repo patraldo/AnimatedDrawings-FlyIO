647652e7b8f8e53cff78e2d8e2a19894249af9f1
ali <alitaquie51@gmail.com>
2025-08-17 16:49:24 -0700

    final push

--- examples/quadruped/main.py
+++
@@ -1,8 +0,0 @@
-import sys
-from io import write_animal_config
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <input_yaml_path>")
-    else:
-        write_animal_config(sys.argv[1])
